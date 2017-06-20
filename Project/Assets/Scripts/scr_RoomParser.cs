2cc040f9eaf0c56ef2e6cac47333ebd3b2c5355e
Quinten Wuyts <quinten.wuyts@student.howest.be>
2017-06-20 16:16:25 +0200

    - Added Base Project

---
+++ Project/Assets/Scripts/scr_RoomParser.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class scr_RoomParser : MonoBehaviour {
+
+    enum ObjectIDs
+    {
+        AIR,
+        WALL,
+        SPIKES,
+        KEY,
+        DOOR,
+        PATROLENEMY,
+        RANGEDENEMY
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Parse(string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.Open);
+        StreamReader reader = new StreamReader(fs);
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            var ids = line.Split(' ');
+            foreach (var idStr in ids)
+            {
+                ObjectIDs id = (ObjectIDs)(int.Parse(idStr));
+                switch (id)
+                {
+                    case ObjectIDs.AIR:
+                        break;
+                    case ObjectIDs.WALL:
+                        break;
+                    case ObjectIDs.SPIKES:
+                        break;
+                    case ObjectIDs.KEY:
+                        break;
+                    case ObjectIDs.DOOR:
+                        break;
+                    case ObjectIDs.PATROLENEMY:
+                        break;
+                    case ObjectIDs.RANGEDENEMY:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
